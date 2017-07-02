20e736c87c07b2f113bc584cc463642bc426804e
Benoit Laurent <benoit.jacques.felix@gmail.com>
2017-07-02 13:49:57 +0200

    add Sliders for articles 2

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  $('.slider-button-next').click(function(){
+
+    // Prépare la prochaine images
+    $('.is-hidden').addClass('right');
+
+    // L'animation
+    setTimeout(function(){
+      $('.is-showing').addClass('left');
+      $('.is-showing').removeClass('is-showing');
+      $('.is-hidden').addClass('is-showing');
+      $('.is-showing').removeClass('is-hidden right');
+    }, 100);
+
+    // Clear Classes
+    setTimeout(function(){
+      $('.left').addClass('is-hidden');
+      $('.is-hidden').removeClass('left');
+    }, 450);
+
+
+
+  });
+
+  $('.slider-button-prev').bind('click', function(){
+
+    // Prépare la prochaine images
+    $('.is-hidden').addClass('left');
+
+    // L'animation
+    setTimeout(function(){
+      $('.is-showing').addClass('right');
+      $('.is-showing').removeClass('is-showing');
+      $('.is-hidden').addClass('is-showing');
+      $('.is-showing').removeClass('is-hidden left');
+    }, 100);
+
+    // Clear Classes
+    setTimeout(function(){
+      $('.right').addClass('is-hidden');
+      $('.is-hidden').removeClass('right');
+    }, 450);
+
+  });
+
+});
